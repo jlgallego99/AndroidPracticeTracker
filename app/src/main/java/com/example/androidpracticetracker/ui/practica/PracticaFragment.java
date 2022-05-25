dc23bed63f699470cfe0bf9a6ba1cc7b19e6bd83
Dunspa <jlgallego99@correo.ugr.es>
2022-05-25 15:55:18 +0200

    Navegacion entre fragments

--- app/src/main/java/com/example/androidpracticetracker/ui/practica/PracticaFragment.java
+++ app/src/main/java/com/example/androidpracticetracker/ui/practica/PracticaFragment.java
@@ -1,4 +1,4 @@
-package com.example.androidpracticetracker.ui.home;
+package com.example.androidpracticetracker.ui.practica;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.androidpracticetracker.databinding.FragmentHomeBinding;
+import com.example.androidpracticetracker.databinding.FragmentPracticaBinding;
 
-public class HomeFragment extends Fragment {
+public class PracticaFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentPracticaBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        PracticaViewModel practicaViewModel =
+                new ViewModelProvider(this).get(PracticaViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentPracticaBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textPractica;
+        practicaViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/androidpracticetracker/ui/practica/PracticaViewModel.java
+++ app/src/main/java/com/example/androidpracticetracker/ui/practica/PracticaViewModel.java
@@ -1,16 +1,16 @@
-package com.example.androidpracticetracker.ui.home;
+package com.example.androidpracticetracker.ui.practica;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class PracticaViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public PracticaViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is Practica fragment");
     }
 
     public LiveData<String> getText() {
