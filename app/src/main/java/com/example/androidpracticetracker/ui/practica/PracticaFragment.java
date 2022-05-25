dc0760565abe4c609e8f3b81c3c7bfcc87d42b7b
Dunspa <jlgallego99@correo.ugr.es>
2022-05-25 22:00:59 +0200

    Esto empieza a funcionar

--- app/src/main/java/com/example/androidpracticetracker/ui/practica/PracticaFragment.java
+++ app/src/main/java/com/example/androidpracticetracker/ui/practica/PracticaFragment.java
@@ -1,6 +1,9 @@
 package com.example.androidpracticetracker.ui.practica;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +12,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.example.androidpracticetracker.R;
 import com.example.androidpracticetracker.databinding.FragmentPracticaBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PracticaFragment extends Fragment {
 
@@ -23,6 +29,25 @@ public class PracticaFragment extends Fragment {
 
         binding = FragmentPracticaBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        FloatingActionButton fab = root.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*NuevaObraFragment fragment = new NuevaObraFragment();
+                FragmentManager fragmentManager = getActivity().getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.nav_host_fragment_activity_main, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();*/
+                Navigation.findNavController(view).navigate(R.id.action_navigation_practica_to_nuevaObra);
+
+                /*fragmentTransaction.add(R.id.fragment_nueva_obra, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                fragmentManager.executePendingTransactions();*/
+            }
+        });
 
         final TextView textView = binding.textPractica;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
