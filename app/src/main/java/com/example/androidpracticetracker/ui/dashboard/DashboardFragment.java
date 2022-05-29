fcdce4ec3ced0b469a14430f49b8088ffb40100b
Dunspa <jlgallego99@correo.ugr.es>
2022-05-29 17:52:01 +0200

    Integrar biblioteca graficas

--- app/src/main/java/com/example/androidpracticetracker/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/androidpracticetracker/ui/dashboard/DashboardFragment.java
@@ -11,21 +11,24 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.androidpracticetracker.databinding.FragmentDashboardBinding;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
 public class DashboardFragment extends Fragment {
-
     private FragmentDashboardBinding binding;
+
+    private LineChart lineChart;
+    private LineData lineData;
+    private LineDataSet lineDataSet;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
